f7cc99d7ad020d8dde90c20ca839f93f7dd80ef8
risnukawirian <risnukawirian1@gmail.com>
2024-11-19 15:22:55 +0800

    Delete KonvensiSuhu.py

--- KonvensiSuhu.py
+++
@@ -1,8 +0,0 @@
-# Meminta input dari pengguna dalam Fahrenheit
-fahrenheit = float(input("Masukkan temperatur dalam Fahrenheit: "))
-
-# Mengonversi Fahrenheit ke Celsius
-celsius = (fahrenheit - 32) * 5 / 9
-
-# Menampilkan hasil konversi
-print(f"{fahrenheit} derajat Fahrenheit = {celsius:.2f} derajat Celsius.")
