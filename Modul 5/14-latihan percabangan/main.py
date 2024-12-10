764d681f44a91c61761d7a4d7f751bc153b322d4
risnukawirian <risnukawirian1@gmail.com>
2024-12-10 12:21:09 +0700

    Add files via upload

---
+++ Modul 5/14-latihan percabangan/main.py
@@ -0,0 +1,8 @@
+# input total belanja 
+total_belanja = int (input("Masukkan total Belanja Anda : Rp "))
+
+# logika hadiah 
+if total_belanja >500000 :
+    print ("Selamat Anda mendapatkan hadiah berupa mouse dan keyboard")
+else :
+    print ("Anda hanya mendapatkan gantungan kunci")
